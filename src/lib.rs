4a2de16e33ef0e1494e8c67597d0b1c97fa39e12
noah the goodra <peterpan0413@live.com>
2021-06-27 13:10:58 -0500

    reorganization

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,5 @@
 use commonlibsse_sys::root::SKSE;
-lazy_static! {
-    static ref SUPPORTED_VERSION: Vec<u32> = vec![0x01050610u32];
-}
+
 
 #[no_mangle]
 pub extern fn lib_test() {
@@ -9,6 +7,9 @@ pub extern fn lib_test() {
 }
 #[no_mangle]
 pub extern fn SKSEPlugin_Query(a_skse:* const SKSE::QueryInterface,a_info:* const SKSE::PluginInfo) -> bool {
+    return query_body(a_skse,a_info);
+}
+fn query_body(a_skse:* const SKSE::QueryInterface,a_info:* const SKSE::PluginInfo) -> bool{
     let askse = unsafe { a_skse.as_ref() }.unwrap();
     unsafe {
         if askse.IsEditor() {
@@ -21,6 +22,10 @@ pub extern fn SKSEPlugin_Query(a_skse:* const SKSE::QueryInterface,a_info:* cons
 
 #[no_mangle]
 pub extern fn SKSEPlugin_Load(pluginter:* const SKSE::LoadInterface) -> bool {
+    return plugin_load_body(pluginter);
+}
+
+fn plugin_load_body(pluginter:* const SKSE::LoadInterface) -> bool {
     let askse = unsafe { pluginter.as_ref() }.unwrap();
     unsafe {
         SKSE::Init(askse);
--- src/main.rs
+++
@@ -1,8 +0,0 @@
-mod lib;
-use crate::lib::*;
-use std::ffi::c_void;
-
-fn main() {
-    lib_test();
-
-}
